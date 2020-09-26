a4510f5630b8d557219b2624d83f9b526d61eb44
Artem <singlelogic@gmail.com>
2020-09-26 22:48:58 +0300

    changed 'create_table' method, added methods selecting and updating data

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ from task import Task
 
 
 task = Task('db.sqlite3', 'tasks')
-# print(task)
-task.add("tasks", "2020-09-17", "First_task")
-# print(task)
-task.add("tasks", "2020-09-18", "Second_task")
-# print(task)
-task.add("tasks", "2020-09-19", "Third_task")
+# task.add_task("2020-09-17", "First_task")
+# task.add_task("2020-09-18", "Second_task")
+# task.add_task("2020-09-19", "Third_task")
+# task.add_task("2020-09-17", "Fourth_task")
+
+for row in task.find_task("2020-09-17"):
+    print(row)
+
+print("#########################")
+for row in task.find_all_tasks():
+    print(row)
+
+task.change_status_task("2020-09-17", "First_task")
+task.change_status_all_task("2020-09-17")
--- task.py
+++ task.py
@@ -2,13 +2,34 @@ from db import Sqlite
 
 
 class Task:
-    def __init__(self, name_db:str, name_table:str):
+    __tablename__ = 'tasks'
+
+    def __init__(self, name_db: str, name_table: str):
         self.db = Sqlite(name_db)
-        self.db.create_table(name_table, ('date text', 'task text', 'done text'))
+        self.db.create_table(self.__tablename__, ('date TEXT', 'task TEXT', 'done TEXT'))
 
     def __str__(self):
         pass
 
-    def add(self, name_table:str, date:str, event:str, done:str='no'):
+    def add_task(self, date: str, event: str, done: str = 'no'):
         data = (date, event, done)
-        self.db.insert_data(name_table, data)
+        self.db.insert_data(self.__tablename__, data)
+
+    def find_task(self, date: str):
+        field = 'date'
+        return self.db.select_data(self.__tablename__, field, date)
+
+    def find_all_tasks(self):
+        return self.db.select_data(self.__tablename__)
+
+    def change_status_task(self, date: str, event: str = '', done: str = 'yes'):
+        """Change the status of the specified task to complete."""
+        _set = f"done='{done}'"
+        where = f"date='{date}' and task='{event}'"
+        self.db.update_data(self.__tablename__, _set, where)
+
+    def change_status_all_task(self, date: str, done: str = 'yes'):
+        """Change the status of tasks to completed on the specified date."""
+        _set = f"done='{done}'"
+        where = f"date='{date}'"
+        self.db.update_data(self.__tablename__, _set, where)
