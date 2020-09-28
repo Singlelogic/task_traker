de445d85c3f73b11238777a7311091a0aba79cb5
Artem <singlelogic@gmail.com>
2020-09-28 16:42:37 +0300

    modified structure of the application

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
-from task import Task
+from model.task import Task
 
 
-task = Task('db.sqlite3', 'tasks')
-# task.add_task("2020-09-17", "First_task")
-# task.add_task("2020-09-18", "Second_task")
-# task.add_task("2020-09-19", "Third_task")
-# task.add_task("2020-09-17", "Fourth_task")
+# task1 = Task('2020-09-17', 'First_task')
+# task1.save()
+#
+# task2 = Task('2020-09-17', 'Second_task')
+# task2.save()
+#
+# task3 = Task('2020-09-18', 'Third_task')
+# task3.save()
+#
+# task4 = Task('2020-09-17', 'Fourth_task')
+# task4.save()
 
-for row in task.find_task("2020-09-17"):
-    print(row)
 
-print("#########################")
-for row in task.find_all_tasks():
-    print(row)
+all_tasks = Task.find_task()
+for task in all_tasks:
+    print(task)
 
-task.change_status_task("2020-09-17", "First_task")
-task.change_status_all_task("2020-09-17")
+print("##########################################")
+task = Task.find_task("2020-09-18")
+for task in task:
+    print(task)
+
+Task.change_status_task('2020-09-17', 'First_task')
+
+print("##########################################")
+all_tasks = Task.find_task()
+for task in all_tasks:
+    print(task)
+
+Task.change_status_task('2020-09-17')
+
+print("##########################################")
+all_tasks = Task.find_task()
+for task in all_tasks:
+    print(task)
---
+++ model/task.py
@@ -0,0 +1,67 @@
+import sqlite3
+
+
+class Task:
+    """This class is used for adding tasks, changing their state,
+    and searching for."""
+    __tablename__ = 'tasks'
+
+    def __init__(self, date: str, event: str, done: str = 'no'):
+        self.__date = date
+        self.__event = event
+        self.__done = done
+
+    def __str__(self):
+        return f"Date: {self.__date} - {self.__event}; Done: {self.__done}"
+
+    def save(self) -> None:
+        """Method for saving an object in the database"""
+        conn = sqlite3.connect('db.sqlite3')
+        cur = conn.cursor()
+
+        query_insert = f"INSERT INTO {self.__tablename__} VALUES(NULL, ?, ?, ?)"
+        cur.execute(query_insert, (self.__date, self.__event, self.__done))
+
+        conn.commit()
+        conn.close()
+
+    @classmethod
+    def find_task(cls, date: str = '') -> list:
+        """To find the tasks from the database.
+
+        If a date is specified, it returns data for that date.
+        If no date is specified, it returns all tasks.
+        """
+        conn = sqlite3.connect('db.sqlite3')
+        cur = conn.cursor()
+
+        if date:
+            query_select = f"SELECT * FROM {cls.__tablename__} WHERE date='{date}'"
+        else:
+            query_select = f"SELECT * FROM {cls.__tablename__}"
+        tasks = []
+        for row in cur.execute(query_select):
+            tasks.append(cls(row[1], row[2], row[3]))
+        conn.close()
+        return tasks
+
+    @classmethod
+    def change_status_task(cls, date: str, event: str = '', done: str = 'yes') -> None:
+        """Changing the task status.
+
+        By default, the task status changes to completed.
+        If no task is specified, this changes the status of all tasks for
+        the specified date.
+        """
+        conn = sqlite3.connect('db.sqlite3')
+        cur = conn.cursor()
+
+        if event:
+            query_update = f"UPDATE {cls.__tablename__} SET done=? WHERE date=? and task=?"
+            cur.execute(query_update, (done, date, event))
+        else:
+            query_update = f"UPDATE {cls.__tablename__} SET done=? WHERE date=?"
+            cur.execute(query_update, (done, date))
+
+        conn.commit()
+        conn.close()
