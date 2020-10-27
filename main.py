39251d7a11980eac8e934ae5e48f3ba1fd89e853
Singlelogic <singlelogic@gmail.com>
2020-10-27 10:30:38 +0300

    Changed the structure of launching the 'task tracker' in module 'main'

--- main.py
+++ main.py
@@ -7,43 +7,47 @@ from views.commands import (
 
 
 def task_tracker_commands():
-    """Task tracker commands."""
+    """Task tracker commands. Entry point to the "task tracker."""
     command = ''
     print("Enter 'StartApp' to start working with the task tracker ('Quit' - to exit):")
 
     while command != 'Quit':
         command = input('> ')
-
         if command == 'StartApp':
-            print("Welcome to the task tracker!\nEnter 'Quit' to quit the task tracker. "
-                  "For help enter 'Help'.")
-            _exit = False
-            while not _exit:
-                commands = input('> ').strip().split('\n')
-                for command in commands:
-                    command = command.split()
-                    if command:
-                        if command[0] == 'Help':
-                            print(help_task())
+            start_app()
 
-                        elif command[0] == 'Add':
-                            print(add_task(command))
 
-                        elif command[0] == 'Del':
-                            print(del_task(command))
+def start_app():
+    """Getting started with task tracker."""
+    print("Welcome to the task tracker!\nEnter 'Quit' to quit the task tracker. "
+          "For help enter 'Help'.")
+    _exit = False
+    while not _exit:
+        commands = input('> ').strip().split('\n')
+        for command in commands:
+            command = command.split()
+            if command:
+                if command[0] == 'Help':
+                    print(help_task())
 
-                        elif command[0] == 'Find':
-                            print(find_task(command))
+                elif command[0] == 'Add':
+                    print(add_task(command))
 
-                        elif command[0] == 'Print':
-                            print(print_task(command))
+                elif command[0] == 'Del':
+                    print(del_task(command))
 
-                        elif command[0] == 'Quit':
-                            print(quit_task())
-                            _exit = True
+                elif command[0] == 'Find':
+                    print(find_task(command))
 
-                    if _exit:
-                        break
+                elif command[0] == 'Print':
+                    print(print_task(command))
+
+                elif command[0] == 'Quit':
+                    print(quit_task())
+                    _exit = True
+
+            if _exit:
+                break
 
 
 @click.command()
