7441f36be2790d1f441722e5c8b6e15c9b9b4202
Singlelogic <singlelogic@gmail.com>
2020-10-27 10:38:04 +0300

    Changed the structure of the main module

--- main.py
+++ main.py
@@ -6,6 +6,24 @@ from views.commands import (
 )
 
 
+@click.command()
+@click.option('--show_current', is_flag=True, help='Show all current not completed tasks')
+@click.option('--show_done', is_flag=True, help='Show all current completed tasks')
+@click.option('--database', is_flag=True, help='Path to the used database')
+def main(show_current, show_done, database):
+    """A personal task recorder that communicates with the user via standard I/O streams."""
+    # Command line interface
+    if show_current:
+        print(cli_show_current())
+    elif show_done:
+        print(cli_show_done())
+    elif database:
+        print(path_database())
+    else:
+        # Task tracker commands
+        task_tracker_commands()
+
+
 def task_tracker_commands():
     """Task tracker commands. Entry point to the "task tracker."""
     command = ''
@@ -29,43 +47,19 @@ def start_app():
             if command:
                 if command[0] == 'Help':
                     print(help_task())
-
                 elif command[0] == 'Add':
                     print(add_task(command))
-
                 elif command[0] == 'Del':
                     print(del_task(command))
-
                 elif command[0] == 'Find':
                     print(find_task(command))
-
                 elif command[0] == 'Print':
                     print(print_task(command))
-
                 elif command[0] == 'Quit':
                     print(quit_task())
                     _exit = True
-
             if _exit:
                 break
-
-
-@click.command()
-@click.option('--show_current', is_flag=True, help='Show all current not completed tasks')
-@click.option('--show_done', is_flag=True, help='Show all current completed tasks')
-@click.option('--database', is_flag=True, help='Path to the used database')
-def main(show_current, show_done, database):
-    """A personal task recorder that communicates with the user via standard I/O streams."""
-    # Command line interface
-    if show_current:
-        print(cli_show_current())
-    elif show_done:
-        print(cli_show_done())
-    elif database:
-        print(path_database())
-    else:
-        # Task tracker commands
-        task_tracker_commands()
 
 
 if __name__ == "__main__":
